e455bf2e6cab7e28a3efc14d183e8a150dc51345
Pedro Santos <99020483+pehenriquea@users.noreply.github.com>
2023-05-03 08:58:37 -0300

    Update app_pad06.dart

--- app_pad06.dart
+++ app_pad06.dart
@@ -32,32 +32,166 @@ class _MyAppState extends State<_MyApp> {
 
 //home
 import 'package:flutter/material.dart';
-import 'package:app_pad/home.dart';
 import 'package:flutter/services.dart';
+import 'package:app_pad/sliders.dart';
+import 'package:app_pad/button.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+//flutter pub add flutter_slider_drawer
 
-void main() => runApp(_MyApp());
+class Home extends StatelessWidget {
+  const Home({Key? key, required this.r, }) : super(key: key);
+  final int r;
+  final String title = "AppPad";
 
-class _MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<_MyApp> {
-
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+  String percentageModifier(double value) {
+    return '';
   }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "AppPad",
-      debugShowCheckedModeBanner: false,
-      home: Home(
-          r: 0xFF000000
-      ),
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]
+    );
+
+    void alerta(){
+      showDialog(
+          context: context,
+          builder: (content){
+            return const AlertDialog(
+              title: Text(textAlign: TextAlign.center,"Sobre o APP"),
+              content: Text(textAlign: TextAlign.center, "Criado por Pedro Santos\nCopyright 2023"),
+            );
+          });
+    }
+
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: FractionallySizedBox(
+          child: SliderDrawer(
+              slideDirection: SlideDirection.RIGHT_TO_LEFT,
+              appBar: SliderAppBar(
+                  drawerIconSize: height * 0.07,
+                  appBarPadding: EdgeInsets.only(right: width * 0.06, top: height * 0),
+                  isTitleCenter: false,
+                  appBarColor: Colors.grey.shade200,
+                  appBarHeight: height * 0.14,
+                  title: Text(title,
+                      textScaleFactor: height * 0.004,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w700
+                      )
+                  )
+              ),
+              slider: Container(
+                color: Colors.grey.shade200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: height * 0.05),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          sliderhigh(),
+                          slidermid(),
+                          sliderlow(),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: height * 0.05),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                              onPressed: alerta,
+                              child: const Text(
+                                  style: TextStyle(
+                                      color: Colors.black
+                                  ),
+                                  "Sobre o App")),
+                          const Icon(
+                              Icons.info_outlined),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              child: Container(
+                  color: Color(r),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: const [
+                              button(
+                                texto: "C",
+                                path: "assets/audios/C Pad.mp3",
+                              ),
+                              button(
+                                  texto: "C#",
+                                  path: "assets/audios/CSharp Pad.mp3",
+                              ),
+                              button(
+                                  texto: "D",
+                                  path: "assets/audios/D Pad.mp3",
+                              ),
+                              button(
+                                  texto: "D#",
+                                  path: "assets/audios/DSharp Pad.mp3",
+                              ),
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: const [
+                              button(
+                                  texto: "E",
+                                  path: "assets/audios/E Pad.mp3",
+                              ),
+                              button(
+                                  texto:  "F",
+                                  path: "assets/audios/F Pad.mp3",
+                              ),
+                              button(
+                                  texto: "F#",
+                                  path: "assets/audios/FSharp Pad.mp3",
+                              ),
+                              button(
+                                  texto: "G",
+                                  path: "assets/audios/G Pad.mp3",
+                              ),
+                            ]),
+
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: const [
+                              button(
+                                  texto: "G#",
+                                  path: "assets/audios/GSharp Pad.mp3",
+                              ),
+                              button(
+                                  texto: "A",
+                                  path: "assets/audios/A Pad.mp3",
+                              ),
+                              button(
+                                  texto: "A#",
+                                  path: "assets/audios/ASharp Pad.mp3",
+                              ),
+                              button(
+                                  texto: "B",
+                                path: "assets/audios/B Pad.mp3",
+                              ),
+                            ]),
+                      ]
+                  )
+              )
+          ),
+        )
     );
   }
 }
